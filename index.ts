ccf0838bc855aaa068149242b6c5ab80ab2d760d
pjo <pjo@dbc.dk>
2022-10-17 14:29:44 +0200

    root get for liveliness probe

--- index.ts
+++ index.ts
@@ -6,6 +6,11 @@ server.get('/ping', async (request, reply) => {
     return 'pong\n'
 })
 
+// for liveliness probe
+server.get('/', async (request, reply) => {
+    return 'ok'
+})
+
 server.listen({ port: 3000 }, (err, address) => {
     if (err) {
         console.error(err)
