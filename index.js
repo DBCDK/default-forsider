1a1fc7328736cc4b13833fe58a64fba56401c139
pjo <pjo@dbc.dk>
2022-10-18 09:15:47 +0200

    listen on all ports

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ server.register(fastifyCORS, corsOptions);
 server.get('/', (request, reply) => __awaiter(void 0, void 0, void 0, function* () {
     return 'ok';
 }));
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
     if (err) {
         console.error(err);
         process.exit(1);
--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ server.get('/', async (request, reply) => {
     return 'ok'
 })
 
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ port: 3000, host: '0.0.0.0'}, (err, address) => {
     if (err) {
         console.error(err)
         process.exit(1)
