52cd71932a0bf8b7f7ce1c1f7d8e234e68ddfa7e
pjo <pjo@dbc.dk>
2022-10-19 13:52:22 +0200

    use sharp

--- svg/svgGenerator.ts
+++ svg/svgGenerator.ts
@@ -1,21 +1,53 @@
 import {promises as Fs} from "fs";
 
-export async function generate(title: string): Promise<string> {
-    let svgAsString = await read();
-    return replaceInSvg(svgAsString, title);
+
+/**
+ * Generate an image from given parameter.
+ * @param svgString
+ */
+export function svg2Image(svgString:Buffer): void {
+    // @TODO filename -- some kind of uid
+    const sharp = require("sharp")
+    sharp(svgString)
+        .png()
+        .toFile('images/output.png', (err: any, info: any) => { console.log(info) });
 }
 
+
+/**
+ * Generate a file. Return url to file.
+ * @param title
+ */
+export async function generate(title: string, materialType: string): Promise<String> {
+    let svgAsString = await read();
+    const buf = Buffer.from(replaceInSvg(svgAsString, title));
+    svg2Image(buf);
+    return generateUrl();
+}
+
+/**
+ * TODO - implement
+ */
+function generateUrl(path="", filename=""):string{
+    return "url";
+}
+
+/**
+ * Replace in template with given values.
+ * @param svg
+ * @param title
+ */
 function replaceInSvg(svg: string, title: string): string {
     return svg.replace("TITLE_TEMPLATE", title);
 }
 
 /**
- * helper function to read the file
+ * helper function to read the (svg) file.
  * @returns {Promise<string>} | String
  */
 async function read() {
     try {
-        return await Fs.readFile("svg/template.svg", {encoding: "utf8"});
+        return await Fs.readFile("images/template.svg", {encoding: "utf8"});
     } catch (e) {
         console.log(`Read  failed:`, {
             error: String(e)
