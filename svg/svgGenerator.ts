4749e29bf892a434f1b3976a147b197242a02425
pjohans <pjohans@gmail.com>
2022-11-10 07:37:25 +0100

    svg templates + text in svg (#4)
    Co-authored-by: pjo <pjo@dbc.dk>

--- svg/svgGenerator.ts
+++ svg/svgGenerator.ts
@@ -1,22 +1,26 @@
 import {promises as Fs} from "fs";
 
-import {formats, IFormats, sizes} from "../utils";
+import {colors, materialTypes, sizes} from "../utils";
 import {IDefaultQuerystring} from "../index";
 
+const _ = require("lodash")
+
 /**
- * Generate a file. Return url to file.
- * TODO - more parameters (format(jpg, svg, gif ...), size(thumbnail, large .. )
+ * Generate a file. Return uid of file. Let graphql api handle url generation.
  * @param query
  */
 export async function generate(query: IDefaultQuerystring): Promise<string> {
     const {title, materialType} = query
+    // we need to generate same hash each time - use 'uuid-by-string' @see https://www.npmjs.com/package/uuid-by-string
+    const getUuid = require('uuid-by-string');
+    const uuidHash = getUuid(`${title}${materialType}`);
     let svgAsString = await read(materialType);
     const buf = Buffer.from(replaceInSvg(svgAsString, title));
     Object.keys(sizes).forEach((size) => {
-        const path = pathToImage(title, materialType, size);
-        svg2Image(buf, path, size);
+        const imagePath = pathToImage(uuidHash, size);
+        svg2Image(buf, imagePath, size);
     })
-    return generateUrl("fisk");
+    return generateUrl(uuidHash);
 }
 
 /**
@@ -26,13 +30,15 @@ export async function generate(query: IDefaultQuerystring): Promise<string> {
  * @param format -defaults to 'svg'
  * @param size
  */
-function svg2Image(svgString: Buffer, path: string, size:string): void {
-    // @TODO filename -- some kind of uid
+function svg2Image(svgString: Buffer, path: string, size: string): void {
     const sharp = require("sharp")
-    const sizes = size === "large" ? {width:150, height:300} : {width:75, height:150}
+    const sizes = size === "large" ? {width: 300, height: 460} : {width: 75, height: 115}
     sharp(svgString)
         .resize(sizes)
-        .toFormat("jpg")
+        .jpeg({
+            width: sizes.width,
+            height: sizes.height,
+        })
         .toFile(`${path}.jpg`, (err: any, info: any) => {
             console.log(err, "ERROR")
             console.log(info, "INFO")
@@ -40,35 +46,72 @@ function svg2Image(svgString: Buffer, path: string, size:string): void {
 }
 
 /**
- * TODO implement
  * Return path of the file to be written.
  * @param title
  * @param materialType
  * @param size
  */
-function pathToImage(title: string, materialType:string, size: string): string {
-    // we need to generate same hash each time - use 'uuid-by-string' @see https://www.npmjs.com/package/uuid-by-string
-    const getUuid = require('uuid-by-string');
-    const uuidHash = getUuid(`${title}${materialType}`);
+function pathToImage(uuidHash: string, size: string): string {
     return `images/${size}/${uuidHash}`;
 }
 
 /**
- * TODO - implement
- * Return url to the image being generated.
+ * Return unique id of to the image generated.
  */
 function generateUrl(path = "", filename = ""): string {
     return path;
 }
 
 /**
- * Replace in template with given values.
+ * Get a random color from colors enum.
+ */
+function randomColor(): string {
+    const items = Object.values(colors);
+    const keys = Object.keys(items);
+    const random: number = +keys[Math.floor(Math.random() * keys.length)];
+
+    return items[random];
+}
+
+/**
+ * Replace in template with given values and a random fill color.
+ * Handle and insert title af cover.
  * @param svg
  * @param title
  */
 function replaceInSvg(svg: string, title: string): string {
-    // @TODO random colors from colors
-    return svg.replace("TITLE_TEMPLATE", title);
+    const svgColor = randomColor();
+    // split string if it is to long
+    const lines = splitString(title);
+    // insert each part of string in <tspan> element
+    const svgTitle = lines.map((line)=>'<tspan x="50%" dy="1.2em">' + line + '</tspan>').join(' ');
+    return svg.replace("TITLE_TEMPLATE", svgTitle).replace("COLOR_TEMPLATE", svgColor);
+}
+
+/**
+ * Attempt to split a long title...
+ *     // NOTE .. approximately 24 characters / line ..
+ *
+ * @param longTitle
+ */
+function splitString(longTitle: string): Array<string> {
+    const parts = longTitle.split(' ');
+    const arrayToReturn: Array<string> = [];
+
+    let line: string = "";
+    let globalIndex = 0;
+    parts.forEach((part, index) => {
+        if (line.length + part.length > 23) {
+            arrayToReturn.push(line)
+            line = "";
+            globalIndex = index;
+        }
+        line += ' ' + part;
+    })
+    // the rest of the text
+    arrayToReturn.push(parts.splice(globalIndex).join(' '));
+
+    return arrayToReturn.length > 0 ? arrayToReturn : [longTitle];
 }
 
 /**
@@ -77,7 +120,9 @@ function replaceInSvg(svg: string, title: string): string {
  */
 async function read(materialType: string): Promise<string> {
     try {
-        return await Fs.readFile(`images/template.svg`, {encoding: "utf8"});
+        const index: number = Object.keys(materialTypes).indexOf(_.upperFirst(materialType));
+        const matType = Object.values(materialTypes)[index];
+        return await Fs.readFile(`templates/${matType}.svg`, {encoding: "utf8"});
     } catch (e) {
         console.log(`Read  failed:`, {
             error: String(e)
