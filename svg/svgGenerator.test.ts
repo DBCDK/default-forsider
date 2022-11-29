226bb957a9b7f57d5fbd1355d4fccc8a43affbe3
pjo <pjo@dbc.dk>
2022-11-29 09:38:24 +0100

    some more text thing

--- svg/svgGenerator.test.ts
+++ svg/svgGenerator.test.ts
@@ -1,45 +1,46 @@
-import {splitString} from './svgGenerator'
+import { splitString } from "./svgGenerator";
 
 const maxLength = 22;
 
-
 test("very long word", () => {
-    const longTitle = "dethererenmagetlangstrengpåover22tegn";
-    const actual = splitString(longTitle);
-    const expected = ["dethererenmagetlangst-",
-        "rengpåover22tegn"]
-    expect(actual).toEqual(expected);
-})
+  const longTitle = "dethererenmagetlangstrengpåover22tegn";
+  const actual = splitString(longTitle);
+  const expected = ["dethererenmaget-", "langstrengpåove-", "r22tegn"];
+  expect(actual).toEqual(expected);
+});
 
 test("very very long word", () => {
-    const longTitle = "dethererenmagetlangstrengpåover22tegndethererenmagetlangstrengpåover22tegn";
-    const actual = splitString(longTitle);
-    const expected = ["dethererenmagetlangst-",
-        "rengpåover22tegndethe-",
-        "rerenmagetlangstrengp-",
-        "åover22tegn"];
-    expect(actual).toEqual(expected);
-})
+  const longTitle =
+    "dethererenmagetlangstrengpåover22tegndethererenmagetlangstrengpåover22tegn";
+  const actual = splitString(longTitle);
+  const expected = [
+    "dethererenmaget-",
+    "langstrengpåove-",
+    "r22tegndetherer-",
+    "...",
+  ];
+  expect(actual).toEqual(expected);
+});
 
 test("very long title", () => {
-    const longTitle = "Harry potter og hemmelighedernes kammer";
-    const actual = splitString(longTitle);
-    const expected = ["Harry potter og hemme-",
-        "lighedernes kammer"];
+  const longTitle = "Harry potter og hemmelighedernes kammer";
+  const actual = splitString(longTitle);
+  const expected = ["Harry potter og-", " hemmelighedern-", "es kammer"];
 
-    expect(actual).toEqual(expected);
-})
+  expect(actual).toEqual(expected);
+});
 
 test("very long title with very long word in the end", () => {
-    const longTitle = "Harry potter og hemmelighedernes kammer dethererenmagetlangstrengpåover22tegn";
+  const longTitle =
+    "Harry potter og hemmelighedernes kammer dethererenmagetlangstrengpåover22tegn";
 
-    const actual = splitString(longTitle);
-    const expected = ["Harry potter og hemme-",
-        "lighedernes kammer de-",
-        "thererenmagetlangstre-",
-        "ngpåover22tegn"];
+  const actual = splitString(longTitle);
+  const expected = [
+    "Harry potter og-",
+    " hemmelighedern-",
+    "es kammer dethe-",
+    "...",
+  ];
 
-    expect(actual).toEqual(expected);
-})
-
-
+  expect(actual).toEqual(expected);
+});
